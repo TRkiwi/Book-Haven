3b66fc0bbc096bee018d221507eccc373e5b50c9
Kiwi <abbodi.skaff@gmail.com>
2023-04-18 10:05:39 +0200

    Finilised the HTML for the welcoming page, added more CSS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*----- login form -------- */
+
+var loginForm = document.querySelector('.login-form-container'); // Select the login form container element
+
+// Add event listener to the login button with ID "login-btn"
+document.querySelector("#login-btn").onclick = () =>{
+     // Toggle the "active" class on the login form container element
+    loginForm.classList.toggle("active");
+}
+// Add event listener to the close button with ID "close-login-btn"
+document.querySelector("#close-login-btn").onclick = () =>{
+
+     // Remove the "active" class from the login form container element
+    loginForm.classList.remove("active");
+}
